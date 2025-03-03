9eeead57e11cc102bea27900f52bc7949171e91b
Abhishek4july <abhi0407july@gmail.com>
2025-03-03 20:38:40 +0530

    merged

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -4,11 +4,11 @@ const conf = {
     appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
     appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
     appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
-<<<<<<< HEAD
-    tinyMCEKey: String(import.meta.env.VITE_APIKEY_TINYMCE)
-=======
+
+    tinyMCEKey: String(import.meta.env.VITE_APIKEY_TINYMCE),
+
     appwriteApiKey:String(import.meta.env.VITE_APPWRITE_APIKEY)
->>>>>>> afc6c55244a8c61c2f2b1262a4ef13e476f4b9f3
+
 }
 
 
